a816ff245632fc0b3f4996b4e6856f5f0518de10
godeline <godelineboy@gmail.com>
2022-01-04 19:42:02 +0100

    challenge springJPA3

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,14 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 
 @Entity
 public class Course {
@@ -12,6 +17,9 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
 
     public Course() {
     }
@@ -24,7 +32,15 @@ public class Course {
         return id;
     }
 
-    public void setId(Long id) {
+    public List<Wizard> getWizards() {
+		return wizards;
+	}
+
+	public void setWizards(List<Wizard> wizards) {
+		this.wizards = wizards;
+	}
+
+	public void setId(Long id) {
         this.id = id;
     }
 
